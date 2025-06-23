a3407ef8253037bfd54b013c2afce679fae7a0c4
choi dong geun <dgsb1406@gmail.com>
2025-06-23 16:26:03 +0900

    0623 자바

---
+++ app.js
@@ -0,0 +1,21 @@
+var a = '변수값';
+console.log(a); // print(a)
+
+let b = 1
+b = 2
+console.log(b)
+
+const c = 5
+// c = 10 
+//console.log(c)
+
+//let d = 10
+//console.log(d, typeof d) 
+//d = '문자'
+//console.log(d, typeof d)
+
+if(1 == 0) {
+    console.log("참")
+} else {
+    console.log("거짓")
+}
